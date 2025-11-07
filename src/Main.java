01ec3e719027d6566944836768cf20071c3b5ff7
meriembabori <baboribabori13@gmail.com>
2025-11-07 21:46:26 +0100

    Modifications du distributeur automatique - Afficherstock

--- src/Main.java
+++ src/Main.java
@@ -67,28 +67,30 @@ public class Main {
         int montant = scanner.nextInt();
         scanner.nextLine();
 
-        if(montant < prix.get(nombreProd-1)) {
+        if (montant < prix.get(nombreProd - 1)) {
             System.out.print("le montant est insuffisant ! ");
 
-        }else {
+        } else {
 
-                System.out.println("Vous avez acheté : " + produits.get(nombreProd-1) );
-                int reste = (int) (montant - prix.get(nombreProd - 1));
-                System.out.println("Monnaie rendue :" + reste + " MAD");
-                int getValue = stock.get(nombreProd - 1);
-               stock.set(nombreProd - 1, getValue-1 ) ;
-
-
-        }
-
-
-
-    }
-
-    static void afficherStock(ArrayList<Integer> stock) {
-        for(int i=0; i<stock.size(); i++){
-            System.out.println(stock.get(i));
+            System.out.println("Vous avez acheté : " + produits.get(nombreProd - 1));
+            int reste = (int) (montant - prix.get(nombreProd - 1));
+            System.out.println("Monnaie rendue :" + reste + " MAD");
+            int getValue = stock.get(nombreProd - 1);
+            stock.set(nombreProd - 1, getValue - 1);
         }
     }
 
+    static ArrayList<Integer> stockInitial = new ArrayList<>(List.of(10, 5, 7, 3));
+    static void afficherStock(ArrayList<String> produits, ArrayList<Integer> stock) {
+        System.out.println("Affichage de stock ");
+        for (int i = 0; i < stock.size(); i++) {
+            if (stock.get(i) == 0) {
+                stock.set(i, stockInitial.get(i));
+                System.out.println(produits.get(i) + " était épuisé");
+                System.out.println("remplissage de stock " + stock.get(i));
+            } else {
+                System.out.println(produits.get(i) + " → " + stock.get(i) + " restant");
+            }
+        }
+    }
 }
