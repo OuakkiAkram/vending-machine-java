249e4f5f282ecd10edc8390377b78808b18c6ba8
Ouakki Akram <akramouak6@gmail.com>
2025-11-07 11:24:25 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Distributeur Automatique ===");
+        System.out.println("1. Afficher les produits ");
+        System.out.println("2. Acheter un produit");
+        System.out.println("3. Quitter");
+    }
+}
